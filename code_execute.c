8bf3d5748d77c01dbd466eaf81afe85035f10171
HichamZineb <hichamelamine@hotmail.com>
2023-06-22 09:29:58 -0700

    update swap

--- code_execute.c
+++ code_execute.c
@@ -15,6 +15,7 @@ void code_execute(char *arg_v, stack_t **stack, unsigned int line_number)
 		{"pall", pall},
 		{"pint", pint},
 		{"pop", pop},
+		{"swap", swap},
 		{NULL, NULL}
 	};
 	int i;
--- monty.h
+++ monty.h
@@ -67,5 +67,6 @@ stack_t *add_node(stack_t **stack, const int data);
 void pall(stack_t **stack, unsigned int line_number);
 void pint(stack_t **stack, unsigned int line_number);
 void pop(stack_t **stack, unsigned int line_number);
+void swap(stack_t **stack, unsigned int line_number);
 
 #endif /* MONTY_H */
