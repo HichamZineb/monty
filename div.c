5bf3fd997bca2d67b3971118d8884f38ddb6c689
HichamZineb <hichamelamine@hotmail.com>
2023-06-22 09:51:43 -0700

    update div

---
+++ div.c
@@ -0,0 +1,31 @@
+#include "monty.h"
+
+/**
+ * div - Divides the second top element of the stack by the top element.
+ * @stack: Double pointer to stack.
+ * @line_number: Number of line.
+ *
+ * Return: Nothing
+ */
+void div(stack_t **stack, unsigned int line_number)
+{
+	int result;
+	stack_t *temp;
+
+	if (*stack == NULL || (*stack)->next == NULL)
+	{
+		fprintf(stderr, "L%d: can't div, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+
+	temp = *stack;
+	if (temp->n == 0)
+	{
+		fprintf(stderr, "L%d: division by zero\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	result = temp->next->n / temp->n;
+	temp->next->n = result;
+	*stack = temp->next;
+	free(temp);
+}
